ae9e80fb06afb91b6e71a9618c59c4c8420d8c38
Mohan Ratnam <mosesratnam@yahoo.com>
2023-10-02 19:59:34 +0100

    A C# Exercise 4.2 - Arrays

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Exercise4._2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("");
+        Console.WriteLine("Welcome to Exercise 4.2 - Times Table");
+        Console.WriteLine("=================================");
+        Console.WriteLine("");
+
+        Console.Write("Please type a number greater than 1: ");
+        int userInput;
+        int result;
+        if(int.TryParse(Console.ReadLine(), out userInput)){
+            int[] numbers = new int[userInput];
+            
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = i + 1;
+            }
+
+            foreach (int number in numbers)
+            {
+                if(number > 1){
+                    Console.WriteLine("");
+                    Console.WriteLine("{0} times-table", number);
+                    for (int i = 1;i <= 12; i++) 
+                    {
+                        result = i * number;
+                        Console.WriteLine("{0} * {1} = {2}", i, number, result);
+                    } 
+                }                 
+            }
+        }
+    }
+}
